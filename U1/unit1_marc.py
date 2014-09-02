647310474cfa6a609a44e822c1ec32049148cd18
flaxatives <flax@flaxatives.com>
2014-09-02 18:05:22 -0400

    refine search to normalize case, alphanum, and len > 5
    also format printing

--- U1/unit1_marc.py
+++ U1/unit1_marc.py
@@ -13,19 +13,19 @@ if len(sys.argv) > 1 and os.path.exists(sys.argv[1]):
 # Raw Frequent Words #
 ######################
 # read each file and put tokens into a list
+stopwords = nltk.corpus.stopwords.words('english')
 tokens = []
 for file in os.listdir(dir):
     raw = open(os.path.join(dir, file)).read()
     raw = raw.decode('utf8')
     t = nltk.word_tokenize(raw)
-    t = [w.lower() for w in t]    # make words lowercase
+    # normalized case, alphanumeric words of length > 5
+    t = [w.lower() for w in t if w.lower() not in stopwords and w.isalnum() and len(w) > 5]
     tokens += t
 
 # plot the frequency distribution of the tokens
 fdist1 = nltk.FreqDist(tokens)
 print("Top 20 Words")
 for word in fdist1.most_common(20):
-    print(word)
+    print("{word:<15} {count}".format(word=word[0],count=word[1]))
 fdist1.plot(50, cumulative=True)
-
-
