5468d0a8e540e1c1d35aff51a6e8982c9c1516c7
flax <marcato@vt.edu>
2014-09-19 19:38:24 -0400

    add mapper

--- U2/mapper.py
+++ U2/mapper.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 
 """
 Mapper - 7.3: Wordlength Distribution
@@ -7,16 +7,23 @@ Mapper - 7.3: Wordlength Distribution
 """
 
 import sys
-import nltk
+import zipimport
 
-stopwords = nltk.corpus.stopwords.words('english')
+importer = zipimport.zipimporter('nltk.mod')
+nltk = importer.load_module("nltk")
+
+nltk.data.path += ["./nltkData/"]
+from nltk.corpus import stopwords
+
+stopwords = stopwords.words('english')
 #lines = open(sys.argv[1]).read().splitlines()
 #stopwords = set([l.strip().lower() for l in lines])
 
 for line in sys.stdin:
     line = line.decode('utf8')
     words = nltk.word_tokenize(line)
-    wordlengths = [len(w) for w in words if w not in stopwords]
+    words = [w.lower() for w in words if w not in stopwords]
 
-    for length in wordlengths:
-        print("{0}\t{1}".format(length, 1));
+    for word in words:
+	value = 0 if word in stopwords else 1
+        print("{0}\t{1}".format(word.encode('utf-8'), value));
