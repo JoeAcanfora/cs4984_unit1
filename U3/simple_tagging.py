2e4dd4b10cb586979c4faa2a4e12ddd9305672b6
davidkeimig <davidkeimig@Davids-MacBook-Pro-3.local>
2014-09-24 17:03:51 -0400

    added
    addded

---
+++ U3/simple_tagging.py
@@ -0,0 +1,57 @@
+from __future__ import division
+
+import nltk
+from nltk.corpus import brown
+from nltk.corpus import reuters
+from nltk.corpus import state_union
+import sys
+import glob
+import os
+
+from nltk.corpus import wordnet as wn
+
+from collections import Counter
+from tabulate import tabulate
+
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+import dateutil
+import pyparsing
+import numpy
+import six
+from nltk.corpus import PlaintextCorpusReader
+
+from nltk.tokenize.punkt import PunktSentenceTokenizer, PunktParameters
+from nltk.tokenize import sent_tokenize
+
+corpus_root = '/Users/davidkeimig/Desktop/Islip13Rain_edit'
+wordlists = PlaintextCorpusReader(corpus_root, '.*')
+wordlists.fileids()
+ClassEvent = nltk.Text(wordlists.words())
+
+found_ClassEvent = [w.lower() for w in wordlists.words()]
+
+punkt_param = PunktParameters()
+punkt_param.abbrev_types = set(['dr', 'vs', 'mr', 'mrs', 'prof', 'inc'])
+sentence_splitter = PunktSentenceTokenizer(punkt_param)
+
+dir_path = str(sys.argv[1])
+path = dir_path + "/*.txt"
+list_txt = glob.glob(path)
+
+all_sent = list()
+
+for txt in list_txt:
+	file_y = open(txt).read()
+	tokens = sent_tokenize(file_y)
+	all_sent = all_sent + tokens
+
+for sent in all_sent:
+	text = nltk.word_tokenize(sent)
+	results = nltk.pos_tag(text)
+	print "SENT: "
+	print results
+	print "\n"
+	
