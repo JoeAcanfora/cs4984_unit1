9070aa067ef423613f39ea34ed04efc226e66719
flax <marcato@vt.edu>
2014-09-29 15:03:43 -0400

    add mapper that tokenizes sentences by line and tags

---
+++ U3/mapper.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python2
+
+"""
+Mapper
+
+Split the line into sentences (for now).
+Use tagger to tag POS.
+count occurrences of that word with POS.
+"""
+
+import sys
+import zipimport
+
+try:
+    importer = zipimport.zipimporter('nltk.mod')
+    nltk = importer.load_module("nltk")
+    nltk.data.path += ["./nltkData/"]
+except zipimport.ZipImportError:
+    import nltk
+
+from nltk.tokenize.punkt import PunktSentenceTokenizer, PunktParameters
+from nltk.tokenize import sent_tokenize
+
+from cPickle import load
+
+
+# punkt sentence tokenizer setup
+punkt_param = PunktParameters()
+punkt_param.abbrev_types = set(['dr', 'vs', 'mr', 'mrs', 'prof', 'inc'])
+sentence_splitter = PunktSentenceTokenizer(punkt_param)
+
+# load trained POS tagger from disk
+input = open("t2.pkl", "rb")
+tagger = load(input)
+input.close()
+
+# process data
+for line in sys.stdin:
+    line = line.decode('utf8').lower()
+    sents = sent_tokenize(line)
+
+    for sent in sents:
+        text = nltk.word_tokenize(sent)
+        tagged_sent = tagger.tag(text)
+        for tag in tagged_sent:
+            print("{0}\t{1}\t1".format(tag[0].encode('utf8'),tag[1]))
+
