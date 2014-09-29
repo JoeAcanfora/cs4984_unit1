fb4c8d95b8912e4d4dbbf5fbef0d9abc99aa981d
flax <marcato@vt.edu>
2014-09-29 15:17:01 -0400

    mapper filters out non-alpha words

--- U3/mapper.py
+++ U3/mapper.py
@@ -11,12 +11,16 @@ count occurrences of that word with POS.
 import sys
 import zipimport
 
+
 try:
     importer = zipimport.zipimporter('nltk.mod')
     nltk = importer.load_module("nltk")
     nltk.data.path += ["./nltkData/"]
 except zipimport.ZipImportError:
     import nltk
+
+sys.path.append("..")
+import TextUtils as tu
 
 from nltk.tokenize.punkt import PunktSentenceTokenizer, PunktParameters
 from nltk.tokenize import sent_tokenize
@@ -34,13 +38,14 @@ input = open("t2.pkl", "rb")
 tagger = load(input)
 input.close()
 
-# process data
+# tokenize into sentences and tag
 for line in sys.stdin:
     line = line.decode('utf8').lower()
     sents = sent_tokenize(line)
 
     for sent in sents:
         text = nltk.word_tokenize(sent)
+        text = tu.filter_non_alpha_words(text)
         tagged_sent = tagger.tag(text)
         for tag in tagged_sent:
             print("{0}\t{1}\t1".format(tag[0].encode('utf8'),tag[1]))
