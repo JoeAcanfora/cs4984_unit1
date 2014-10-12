e58529cdd2fa1649199c60655e7c538df80badea
flaxatives <flax@flaxatives.com>
2014-10-12 18:25:22 -0400

    revise reducer to use good classifier

--- U4/reducer_classify.py
+++ U4/reducer_classify.py
@@ -25,6 +25,16 @@ input = open(classifier_file, "rb")
 classifier = load(input)
 input.close()
 
+stopset = []
+with open('../ref/english.stop') as f:
+    mit = f.read().split('\n')
+    stopset = set(nltk.corpus.stopwords.words('english') + mit)
+
+uselesswords = []
+with open('../ref/chinafiltwords.txt') as f:
+    raw = f.read()
+    uselesswords = [line.strip() for line in f.read().split('\n')]
+
 # treat each line as "# filename.txt"
 for line in sys.stdin:
     # read file and classify
@@ -32,24 +42,27 @@ for line in sys.stdin:
     file = open(filename.strip())
     raw = file.read()
     file.close()
-    raw = raw.decode('utf8').lower()
-
+    raw = raw.decode('utf8', 'replace')
     tokens = nltk.word_tokenize(raw)
-    tokens = [word.encode('utf8') for word in tokens]
+    tokens = [word.encode('utf8').lower() for word in tokens]
 
-    pos = 0
-    neg = 0
-    for word in tokens:
-        word = word.lower()
-        type = classifier.classify({'word': word})
-        if type == 'pos':
-            pos += 1
-        elif type == 'neg':
-            neg += 1
+    # filter out unclassifiable words
+
+    tokens = [w for w in tokens if not w in stopset and not w.isdigit() and w.isalpha()
+            and not w in uselesswords]
+    fdist = nltk.FreqDist(tokens)
+    
+    features = {}
+    for item in fdist.most_common(40):
+        word = item[0]
+        count = item[1]
+        features[word] = count
+        
+    type = classifier.classify(features)
 
     # print result and write to appropriate file
-    type = 'positive' if pos > neg else 'negative'
-    outfile = 'relevant.txt' if pos > neg else 'noise.txt'
-    print("{0}\t{1}".format(filename, type))
+    outfile = 'relevant.txt' if type == 'pos' else 'noise.txt'
+    type_name = 'positive' if type == 'pos' else 'negative'
+    print("{0}\t{1}".format(filename, type_name))
     with open(outfile, 'a') as myfile:
         myfile.write(filename)
