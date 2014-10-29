65b9fc41e3b1cee9f8c2ba359f57c2d5108da852
flax <marcato@vt.edu>
2014-10-29 16:33:45 -0400

    add stopword filtering

--- U6/topic.py
+++ U6/topic.py
@@ -15,13 +15,24 @@ import glob
 
 #Call the NLTK stop words list
 stoplist = stopwords.words('english')
+extra_stop = [ 
+#                "../ref/chinafiltwords.txt",
+                "../ref/english.stop"
+             ]
+
+for file in extra_stop:
+    with open(file) as f:
+        raw = f.read().strip()
+        tokens = [word for word in nltk.word_tokenize(raw)]
+        stoplist += tokens
 
 documents = []
+
 for filename in glob.glob(sys.argv[1] + "/*.txt"):
     # tokenize each file
     with open(filename) as f:
         raw = f.read().strip().decode('utf-8')
-        tokens = [word for word in nltk.word_tokenize(raw) if word.lower() not in
+        tokens = [word.lower() for word in nltk.word_tokenize(raw) if word.lower() not in
             stoplist and word.lower().isalpha()]
         documents.append(tokens)
 
@@ -30,15 +41,15 @@ texts = documents
 
 #Build the dictionary and the corpus
 dictionary = corpora.Dictionary(texts)
-print(dictionary)
 corpus = [dictionary.doc2bow(text) for text in texts]
 
 #Define the LDA model and the number of topics.
-notopics = 3
+notopics = 50
 lda = models.ldamodel.LdaModel(corpus=corpus, id2word=dictionary, num_topics=notopics)
+print(lda[dictionary.doc2bow(texts[0])])
 
 #Printing the topic with their probabilities
 print "\n\n", notopics, "Topics with their corresponding probabilities\n"
 for i in range(0, lda.num_topics):
-    print "Topic", i+1, ":", lda.print_topic(i)
-
+    #print "Topic", i+1, ":", lda.print_topic(i)
+    print "Topic", i+1, ":", " + ".join(word[1] for word in lda.show_topic(i))
