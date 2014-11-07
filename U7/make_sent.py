e799c93be2b7532213c0e4ef4c15a2f40219790a
davidkeimig <davidkeimig@Davids-MacBook-Pro-6.local>
2014-11-07 13:01:34 -0500

    jkh
    njk

--- U7/make_sent.py
+++ U7/make_sent.py
@@ -43,8 +43,38 @@ stopset = set(stopwords.words('english') + mit_stopwords)
 all_train = []
 
 file_list = open('../ref/trainset_small.txt').readlines()
+your_words = open('../ref/yourwords.txt').readlines()
+yourwords_list = set(your_words)
 
-dir_path = '/Users/davidkeimig/Desktop/PAK_FLOOD/Pakistan_Flood'
+def letters(input):
+    valids = []
+    for character in input:
+        if character.isalpha():
+            valids.append(character)
+    return ''.join(valids)
+
+def find_lemma(word):
+	lemma_list = list()
+	synsets = nltk.corpus.wordnet.synsets(word)
+	if len(synsets) == 0 or len(synsets[0].lemma_names()) == 0:
+		return word
+	for l in synsets:
+		lemma_list = lemma_list + l.lemma_names()
+	return lemma_list
+
+synsets = list()
+final_set = list()
+for x in yourwords_list:
+	print "first"
+	sent = letters(x)
+	value = find_lemma(sent)
+	final_set = final_set + value
+
+your_words = list()
+for x in final_set:
+	your_words.append(x.encode("ascii"))
+
+dir_path = '/Users/davidkeimig/Documents/cs4984_unit1/cs4984_unit1/U1/china_flood'
 path = dir_path + "/*.txt"
 list_txt = glob.glob(path)
 all_toks_china = list()
@@ -86,9 +116,14 @@ for x in all_toks_china:
 	x = re.sub('\s+', ' ', x)
 	words = word_tokenize(x)
 	good_toks = [w.lower() for w in words if not w.lower() in stopset and not w.isdigit() and w.isalpha() and len(w) >= 2]
-	sent = "";
-	for r in good_toks:
-		sent = sent + r
-		sent = sent + " "
-	with open("./PakSentFiles/"+ str(count) + ".txt", "a") as file:
-		file.write(sent)
+	once = 0
+	for q in your_words:
+		if q in good_toks:
+			if once == 0:
+				sent = ""
+				for r in good_toks:
+					sent = sent + r
+					sent = sent + " "
+				with open("./ChinaSentFiles/"+ str(count) + ".txt", "a") as file:
+					file.write(sent)
+				once = 1
