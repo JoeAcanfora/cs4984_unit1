4ce613c7550ddba4fdaf61eb13be5b09ea59a1df
flax <marcato@vt.edu>
2014-11-17 19:38:30 -0500

    adds regex for people in people.py
    finish adding affected people

---
+++ U8/people.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import re
+import sys
+from numpy import median
+
+regex_num = "((\d,?)+)"
+
+def find_num(text, keywords):
+    # find things that say "X missing" with 5 words possibly in between
+    keywords = "(" + "|".join(keywords) + ")"
+    matches = re.findall(
+            "(" + regex_num + "\s+([a-zA-Z]+\s+){0,5}" + keywords + ")",
+            text, flags=re.IGNORECASE)
+
+    if len(matches) > 0:
+        nums = [int(match[1].replace(",", "")) for match in matches]
+
+        # return the median
+        return int(median(nums))
+
+    else:
+        return None
+
+
+def find_missing(text):
+    return find_num(text, ["missing"])
+
+
+def find_injured(text):
+    return find_num(text, ["injured", "harmed"])
+
+
+def find_killed(text):
+    return find_num(text, 
+            [   "killed", 
+                "dea(d|ths)", 
+                "bod(y|ies)",
+                "casualt(ies|y)",
+                "fatalit(ies|y)"    ])
+
+
+def find_relocated(text):
+    return find_num(text, ["evacuate", "affect(ed|s)?", "relocate(d)?" ])
+
+
+if __name__ == "__main__":
+    with open(sys.argv[1]) as file:
+        text = file.read()
+        print("Missing: " + str(find_missing(text)))
+        print("injured: " + str(find_injured(text)))
+        print("killed: " + str(find_killed(text)))
+        print("relocated: " + str(find_relocated(text)))
